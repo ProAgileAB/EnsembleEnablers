90bf506a76dfd570e91f82605d45d7a945f80523
Olof Bjarnason <olof.bjarnason@proagile.se>
2022-03-07 16:49:53 +0100

    Add self test and the first enabler

--- build.py
+++ build.py
@@ -1,8 +1,21 @@
 # Uses the self-test idiom
+import json
+
+from pathlib import Path
 
 
 def selftest():
-    assert False, "Left for reader"
+    enablers = load_enablers()
+    for enabler in enablers:
+        assert enabler['name'], "Every enabler has a name"
+        assert len(enabler['symptoms']) >= 1, "Every enabler has at least one symptom"
+        assert enabler['proposal'], "Every enabler has a proposal"
+
+
+def load_enablers():
+    path = Path('enablers.json')
+    text = path.read_text(encoding='utf8')
+    return json.loads(text)
 
 
 def generate():
