c1407733d55249898dbddc622420f7729cfd9c50
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-04-07 21:43:47 +0200

    wip move proposal texts to separate md files

--- enabler.py
+++ enabler.py
@@ -28,12 +28,13 @@ def load_enablers():
     path = ENABLERS_PATH
     text = path.read_text(encoding='utf8')
     raw_list = json.loads(text)
+    for enabler in raw_list:
+        proposal_path = Path(file_path_from_enabler_name(enabler['name']))
+        print(proposal_path)
+        if proposal_path.exists():
+            enabler['proposal'] = proposal_path.read_text(encoding='utf8')
     enabler_dto_list = [Enabler.from_dict(enabler) for enabler in raw_list]
     sorted_enablers = sorted(enabler_dto_list, key=lambda enabler: enabler.name)
-    # for enabler in list_of_enablers:
-    #     proposal_path = Path(file_path_from_enabler_name(enabler['name']))
-    #     if proposal_path.exists():
-    #         enabler['proposal'] = proposal_path.read_text(encoding='utf8')
     return sorted_enablers
 
 
@@ -46,12 +47,13 @@ def file_path_from_enabler_name(enabler_name):
 
 
 def id_from_name(enabler_name):
-    return enabler_name.lower().replace(' ', '-')
+    return enabler_name.lower().replace(' ', '-').replace('.', '').replace(',', '')
 
 
 def enabler_self_tests():
     assert 'data/connect-first.md' == file_path_from_enabler_name("Connect first")
     assert 'connect-first' == id_from_name("Connect first")
+    assert 'yes-and' == id_from_name("Yes, and...")
 
 
 ENABLERS_PATH = Path('data/enablers.json')
