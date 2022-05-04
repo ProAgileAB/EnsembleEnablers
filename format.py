05ebddd6d8a0f43fecf6c43dd896da250be74ebb
Olof Bjarnason <olof.bjarnason@gmail.com>
2022-05-04 11:29:59 +0200

    Run formatting from main script

--- format.py
+++ format.py
@@ -1,14 +1,28 @@
-
 from enabler import ENABLERS_PATH
 
 replace = {
- "'": ["’"],
- r"\"": ["“", "”"]
+    "'": ["’"],
+    r"\"": ["“", "”"]
 }
 
-if __name__ == '__main__':
-    text = ENABLERS_PATH.read_text(encoding='utf8')
+
+def format_text(text):
     for new, olds in replace.items():
         for old in olds:
             text = text.replace(old, new)
-    ENABLERS_PATH.write_text(text, encoding='utf8')
+    return text
+
+
+def format_input_files():
+    format_files([ENABLERS_PATH])
+
+
+def format_files(format_paths):
+    for path in format_paths:
+        text = path.read_text(encoding='utf8')
+        text = format_text(text)
+        path.write_text(text, encoding='utf8')
+
+
+if __name__ == '__main__':
+    format_input_files()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from enabler import enabler_self_tests
+from format import format_input_files
 from generate_html import generate_html
 
 if __name__ == '__main__':
     enabler_self_tests()
+    format_input_files()
     generate_html()
